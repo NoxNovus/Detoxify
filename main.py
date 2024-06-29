24f16da39554f0b9a1c626771867d70d0a5f4a60
Rich Chen <120879346+NoxNovus@users.noreply.github.com>
2024-06-29 15:19:18 -0700

    slightly more

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import create_app, socketio
 from app.moderator.llm_filter import init_LLMchain
 
-app = create_app(debug=True, use_reloader=False)
+app = create_app(debug=False)
 
 if __name__ == '__main__':
     init_LLMchain()
