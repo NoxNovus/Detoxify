69a0d3b71397f6d4702090c87f055ab285d8d9c0
Rich Chen <120879346+NoxNovus@users.noreply.github.com>
2024-06-29 13:29:31 -0700

    initial LLM filtering

--- app/moderator/llm_filter.py
+++ app/moderator/llm_filter.py
@@ -4,63 +4,62 @@ import os
 load_dotenv()
 OCTOAI_API_TOKEN = os.environ["OCTOAI_API_TOKEN"]
 
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.schema import Document
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+chain = None # Set to none by default until init
 
-files = os.listdir("../game_data")
-file_texts = []
-for file in files:
-    with open(f"../game_data/{file}") as f:
-        file_text = f.read()
-    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=512, chunk_overlap=64, 
-    )
-    texts = text_splitter.split_text(file_text)
-    for i, chunked_text in enumerate(texts):
-        file_texts.append(Document(page_content=chunked_text, 
-                metadata={"doc_title": file.split(".")[0], "chunk_num": i}))
+def init_LLMchain():
+    from langchain.text_splitter import CharacterTextSplitter
+    from langchain.schema import Document
+    from langchain_community.embeddings import HuggingFaceEmbeddings
+    from langchain.vectorstores import FAISS
 
-embeddings = HuggingFaceEmbeddings()
+    files = os.listdir("../game_data")
+    file_texts = []
+    for file in files:
+        with open(f"../game_data/{file}") as f:
+            file_text = f.read()
+        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=512, chunk_overlap=64,
+        )
+        texts = text_splitter.split_text(file_text)
+        for i, chunked_text in enumerate(texts):
+            file_texts.append(Document(page_content=chunked_text,
+                    metadata={"doc_title": file.split(".")[0], "chunk_num": i}))
 
-vector_store = FAISS.from_documents(
-    file_texts,
-    embedding=embeddings
-)
+    embeddings = HuggingFaceEmbeddings()
 
-from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
-llm = OctoAIEndpoint(
-        model="meta-llama-3-8b-instruct",
-        max_tokens=1024,
-        presence_penalty=0,
-        temperature=0.1,
-        top_p=0.9,
+    vector_store = FAISS.from_documents(
+        file_texts,
+        embedding=embeddings
     )
 
-from langchain.prompts import ChatPromptTemplate
-template="""You are a chat filter system for an online game. Respond with 'toxic' if the prompt is toxic/rude and 'not toxic' if the prompt is not toxic/rude.
-Question: {question} 
-Context: {context} 
-Answer:"""
-prompt = ChatPromptTemplate.from_template(template)
+    from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
+    llm = OctoAIEndpoint(
+            model="meta-llama-3-8b-instruct",
+            max_tokens=1024,
+            presence_penalty=0,
+            temperature=0.1,
+            top_p=0.9,
+        )
 
-retriever = vector_store.as_retriever()
+    from langchain.prompts import ChatPromptTemplate
+    template="""You are a chat filter system for an online game. Respond with 'toxic' if the prompt is toxic/rude and 'not toxic' if the prompt is not toxic/rude.
+    Question: {question}
+    Context: {context}
+    Answer:"""
+    prompt = ChatPromptTemplate.from_template(template)
 
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
-chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+    retriever = vector_store.as_retriever()
 
-chain.invoke("your whole game was a misclick")
-
-import numpy as np
-
-# Save to file
-# np.save('vector_store.npy', vector_store)
+    from langchain_core.runnables import RunnablePassthrough
+    from langchain_core.output_parsers import StrOutputParser
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
 
 
+def LLM_moderate(msg):
+    result = chain.invoke(msg)
+    return result
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from app import create_app, socketio
+from app.moderator.llm_filter import init_LLMchain
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
+    init_LLMchain()
     socketio.run(app)
