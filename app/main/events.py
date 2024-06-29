84624b5214f552a9c8437f1c72bffd2c3abc6bb3
zylim1128 <zylim@uw.edu>
2024-06-29 12:59:11 -0700

    Fixing UI

--- app/main/events.py
+++ app/main/events.py
@@ -1,37 +1,40 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
-
 from app.moderator.simple_filter import censor_target_words, DEFAULT_SWEAR_WORDS
 from .. import socketio
 
+users = {}  # Dictionary to track users in rooms
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
-    """Sent by clients when they enter a room.
-    A status message is broadcast to all people in the room."""
     room = session.get('room')
-    join_room(room)
-    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+    user = session.get('name')
 
+    if room not in users:
+        users[room] = []
+
+    if user in users[room]:
+        emit('username_taken', {'msg': 'Username already taken. Please choose a different one.'})
+        return
+
+    join_room(room)
+    users[room].append(user)
+    emit('status', {'msg': user + ' has entered the room.'}, room=room)
+    emit('user_list', {'users': users[room]}, room=room)
 
 @socketio.on('text', namespace='/chat')
 def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the room."""
     room = session.get('room')
-
     raw_msg = message['msg']
-
-    # Apply moderation
     moderated_msg = censor_target_words(raw_msg, DEFAULT_SWEAR_WORDS)
-
-    emit('message', {'msg': session.get('name') + ':' + moderated_msg}, room=room)
-
+    emit('message', {'user': session.get('name'), 'msg': moderated_msg}, room=room)
 
 @socketio.on('left', namespace='/chat')
 def left(message):
-    """Sent by clients when they leave a room.
-    A status message is broadcast to all people in the room."""
     room = session.get('room')
+    user = session.get('name')
     leave_room(room)
-    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
+    if room in users and user in users[room]:
+        users[room].remove(user)
+    emit('status', {'msg': user + ' has left the room.'}, room=room)
+    emit('user_list', {'users': users[room]}, room=room)
